764bbfd924423510b80ea87ce92760a52cb39ea2
Ngọc  Bích <daongocbich1305@gmail.com>
2025-03-31 17:54:04 +0700

    add BreakableEntity

---
+++ src/uet/oop/bomberman/entities/BreakableEntity.java
@@ -0,0 +1,78 @@
+/******************************************************************************
+ *
+ *  Dependency: Entity.java
+ *
+ *  The abstract data type for the Breakable Entity in general.
+ *
+ ******************************************************************************/
+
+package uet.oop.bomberman.entities;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public abstract class BreakableEntity extends Entity {
+    // Biến boolean kiểm tra trạng thái của đối tượng
+    public boolean isBroken; // true = đã bị phá hủy
+    protected boolean isExploding; // true = đang nổ
+    protected int animate = 0;
+    protected final int animate_MAX_VALUE = 7500;
+
+
+    public BreakableEntity(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+        isBroken = false;
+        isExploding = false;
+    }
+
+
+    public boolean isBroken() {
+        return isBroken;
+    }
+
+    public void setBroken(boolean broken) {
+        isBroken = broken;
+    }
+
+    public boolean isExploding() {
+        return isExploding;
+    }
+
+
+    public void breakEntity() {
+        isExploding = true;
+        animate = 0;
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                isExploding = false;
+                isBroken = true;
+            }
+        }, 500L);
+    }
+
+
+    @Override
+    public void render(GraphicsContext gc) {
+        if (!isBroken) {
+            super.render(gc);
+        }
+    }
+
+    protected void animate() {
+        if (animate < animate_MAX_VALUE) {
+            animate++;
+        } else animate = 0;
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
+
