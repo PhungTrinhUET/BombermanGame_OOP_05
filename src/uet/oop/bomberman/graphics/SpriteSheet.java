84bd4ba2d6771aaf49ccac66c436783e2df844d9
Ngọc  Bích <daongocbich1305@gmail.com>
2025-04-01 23:32:05 +0700

    create sprite sheet

--- src/uet/oop/bomberman/graphics/SpriteSheet.java
+++ src/uet/oop/bomberman/graphics/SpriteSheet.java
@@ -1,4 +1,13 @@
 package uet.oop.bomberman.graphics;
 
+import java.awt.image.BufferedImage;
+
 public class SpriteSheet {
+
+    private String _path;
+    public final int SIZE;
+    public int[] _pixels;
+    public BufferedImage image;
+
+
 }
