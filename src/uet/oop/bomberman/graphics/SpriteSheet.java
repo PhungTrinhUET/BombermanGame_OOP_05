8c2cbb49ab663b182bedd0f8586696d7ff7451c8
Ngọc  Bích <daongocbich1305@gmail.com>
2025-04-01 23:35:21 +0700

    create sprite sheet

--- src/uet/oop/bomberman/graphics/SpriteSheet.java
+++ src/uet/oop/bomberman/graphics/SpriteSheet.java
@@ -9,5 +9,21 @@ public class SpriteSheet {
     public int[] _pixels;
     public BufferedImage image;
 
+    public static SpriteSheet tiles = new SpriteSheet("/textures/classic.png", 256);
+
+    public SpriteSheet(String path, int size) {
+        _path = path;
+        SIZE = size;
+        _pixels = new int[SIZE * SIZE];
+        load();
+    }
+
+    public void load() {
+        image = SpriteLoader.loadImage(_path);
+        int w = image.getWidth();
+        int h = image.getHeight();
+        _pixels = new int[w * h];
+        image.getRGB(0, 0, w, h, _pixels, 0, w);
+    }
 
 }
