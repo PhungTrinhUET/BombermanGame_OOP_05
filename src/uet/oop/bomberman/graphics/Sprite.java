561a5119a9b0b1e291b5c5c41cace9cd0aaa8620
Ngọc  Bích <daongocbich1305@gmail.com>
2025-04-01 23:43:01 +0700

    create sprite

--- src/uet/oop/bomberman/graphics/Sprite.java
+++ src/uet/oop/bomberman/graphics/Sprite.java
@@ -1,4 +1,20 @@
 package uet.oop.bomberman.graphics;
 
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+
+/**
+ * Lưu trữ thông tin các pixel của 1 sprite (hình ảnh game)
+ */
+
 public class Sprite {
+    public static final int DEFAULT_SIZE = 16;
+    public static final int SCALED_SIZE = DEFAULT_SIZE * 3;
+    private static final int TRANSPARENT_COLOR = 0xffff00ff;
+    public final int SIZE;
+    private int _x, _y;
+    public int[] _pixels;
+    protected int _realWidth;
+    protected int _realHeight;
+    private SpriteSheet _sheet;
 }
