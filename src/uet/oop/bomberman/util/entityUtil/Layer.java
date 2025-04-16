ff4640c66d1c19d21ede0b79b9255eb6f84b6e3c
Ngọc  Bích <daongocbich1305@gmail.com>
2025-04-16 15:49:58 +0700

    set up layer

--- src/uet/oop/bomberman/util/entityUtil/Layer.java
+++ src/uet/oop/bomberman/util/entityUtil/Layer.java
@@ -1,4 +1,26 @@
 package uet.oop.bomberman.util.entityUtil;
 
+import javafx.scene.canvas.GraphicsContext;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+import java.util.Stack;
+
 public class Layer {
+    private int x;
+    private int y;
+    public Stack<Entity> stack = new Stack<>();
+
+    public Layer(int xUnit, int yUnit) {
+        this.x = xUnit * Sprite.SCALED_SIZE;
+        this.y = yUnit * Sprite.SCALED_SIZE;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
