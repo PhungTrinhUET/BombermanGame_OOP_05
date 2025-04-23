ba934132c4ea16f44020854894b6df3d8df4b0a5
PhungTrinhUET <128596518+PhungTrinhUET@users.noreply.github.com>
2025-04-23 14:57:21 +0700

    update Board.java Create Entity getEntityCollideWith

--- src/uet/oop/bomberman/util/gameUtil/Board.java
+++ src/uet/oop/bomberman/util/gameUtil/Board.java
@@ -1,9 +1,16 @@
 package uet.oop.bomberman.util.gameUtil;
 
+import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.character.Enemy;
 
 public class Board {
     public int EnemyAIDirection(Enemy enemy) {
         return 0;
     }
+
+    public Entity getEntityCollideWith(Entity entity, double addX, double addY) {
+        return null;
+    }
 }
+
+
