e89ba6b086900273403e5608b4395b30e6688770
PhungTrinhUET <128596518+PhungTrinhUET@users.noreply.github.com>
2025-05-13 22:51:01 +0700

    update Sound.java - update BGM and SE Sound - update cloneOf method

--- src/uet/oop/bomberman/sound/Sound.java
+++ src/uet/oop/bomberman/sound/Sound.java
@@ -1,10 +1,28 @@
 package uet.oop.bomberman.sound;
 
+import com.sun.media.jfxmedia.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+/**
+ * Abstract class Sound: dùng JavaFX Media API để phát âm thanh - Hoox trợ định dạng MP3 và WAV
+ * Các âm thanh nền (BGM - Background Music):
+ * - titleBGM: Nhạc nền màn hình chính
+ * - stageStartBGM: Nhạc khi bắt đầu level mới
+ * - gameOverBGM: Nhạc khi thua game
+ * - stageClearBgm: Nhạc khi hoàn thành level
+ * - MainBGM: Nhạc nền chính khi chơi game
+ *
+ * Các hiệu ứng âm thanh (SE - Sound Effects):
+ * - explosionSound: Âm thanh nổ bom
+ * - LayBombSound: Âm thanh đặt bom
+ * - powerUpSound: Âm thanh nhận power-up
+ * - bomberisKilledSound: Âm thanh khi Bomberman chết
+ * - allEnemiesIsKilledSound: Âm thanh khi tiêu diệt hết kẻ thù
+ * - endingSE: Âm thanh kết thúc game
+ */
 public abstract class Sound {
-
+    //BGM - Background Music
     public static final MediaPlayer TitleBGM = new MediaPlayer(
             new Media(Sound.class.getResource("/audio/TitleScreen.mp3").toString())
     );
@@ -23,6 +41,8 @@ public abstract class Sound {
             new Media(Sound.class.getResource("/audio/MainBGM.mp3").toString())
     );
 
+
+    //SE - Sound Effects
     public static final MediaPlayer explosionSound = new MediaPlayer(
             new Media(Sound.class.getResource("/audio/ExplosionSE.mp3").toString())
     );
@@ -47,7 +67,28 @@ public abstract class Sound {
             new Media(Sound.class.getResource("/audio/Ending.mp3").toString())
     );
 
+    /**
+     * Phương thức cloneOf:
+     * - Tạo một bản sao của MediaPlayer
+     * - Dùng để phát nhiều âm thanh cùng lúc
+     * - Tránh xung đột khi phát âm thanh
+     */
     public static MediaPlayer cloneOf(MediaPlayer that) {
         return new MediaPlayer(that.getMedia());
     }
 }
+
+/* Cấu trúc thư mục âm thanh
+/audio/
+    TitleScreen.mp3
+    StageStart.mp3
+    GameOver.mp3
+    StageClear.mp3
+    MainBGM.mp3
+    ExplosionSE.mp3
+    LayBombSE.wav
+    PowerUpSE.wav
+    BomberKilledSE.wav
+    KillAllEnemiesSE.wav
+    Ending.mp3
+*/
