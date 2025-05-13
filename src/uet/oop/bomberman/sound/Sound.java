9f4fed747100e365c7015f513d3d847a5fdcfa69
Ngọc  Bích <daongocbich1305@gmail.com>
2025-05-13 07:31:23 +0700

    add sound

--- src/uet/oop/bomberman/sound/Sound.java
+++ src/uet/oop/bomberman/sound/Sound.java
@@ -1,4 +1,49 @@
 package uet.oop.bomberman.sound;
 
-public class Sound {
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public abstract class Sound {
+
+    public static final MediaPlayer TitleBGM = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/TitleScreen.mp3").toString())
+    );
+    public static final MediaPlayer stageStartBGM = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/StageStart.mp3").toString())
+    );
+    public static final MediaPlayer gameOverBGM = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/GameOver.mp3").toString())
+    );
+
+    public static final MediaPlayer stageClearBgm = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/StageClear.mp3").toString())
+    );
+
+    public static final MediaPlayer MainBGM = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/MainBGM.mp3").toString())
+    );
+
+    public static final MediaPlayer explosionSound = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/ExplosionSE.mp3").toString())
+    );
+
+    public static final MediaPlayer LayBombSound = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/LayBombSE.wav").toString())
+    );
+
+    public static final MediaPlayer powerUpSound = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/PowerUpSE.wav").toString())
+    );
+
+    public static final MediaPlayer bomberisKilledSound = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/BomberKilledSE.wav").toString())
+    );
+
+    public static final MediaPlayer allEnemiesIsKilledSound = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/KillAllEnemiesSE.wav").toString())
+    );
+
+    public static final MediaPlayer endingSE = new MediaPlayer(
+            new Media(Sound.class.getResource("/audio/Ending.mp3").toString())
+    );
 }
