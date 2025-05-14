f173eba09d25132df265d34119ada5bf99afc818
PhungTrinhUET <128596518+PhungTrinhUET@users.noreply.github.com>
2025-05-14 19:08:41 +0700

    update class Option.java

--- src/uet/oop/bomberman/display/scene/menu/Option.java
+++ src/uet/oop/bomberman/display/scene/menu/Option.java
@@ -1,4 +1,49 @@
 package uet.oop.bomberman.display.scene.menu;
 
-public class Option {
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import uet.oop.bomberman.Main;
+
+/**
+ * Tham khảo youtuber Almas Baimagambetov.
+ */
+public class Option extends HBox {
+    private ImageView leftArrow;
+    private ImageView rightArrow;
+    private Text text;
+    private Runnable script;
+
+    public Option(String name) {
+        super(15);
+        setAlignment(Pos.CENTER);
+
+        leftArrow = new ImageView(getClass().getResource("/textures/leftarrow.png").toString());
+        rightArrow = new ImageView(getClass().getResource("/textures/rightarrow.png").toString());;
+
+        text = new Text(name);
+        text.setFont(Main.FONT);
+
+        getChildren().addAll(leftArrow, text, rightArrow);
+        setActive(false);
+    }
+
+    public void setActive(boolean active) {
+        leftArrow.setVisible(active);
+        rightArrow.setVisible(active);
+        text.setFill(active ? Color.WHITE : Color.GRAY);
+    }
+
+    public void setOnActivate(Runnable r) {
+        script = r;
+    }
+
+    public void activate() {
+        if (script != null) {
+            script.run();
+        }
+    }
 }
